f4a53cc0e25be09f7016dd5eb0c568c97946a363
jeovane16 <jeovane.araujo@opolen.com.br>
2021-04-15 14:33:35 -0300

    adionado os métodos de cause

--- lib/polen_charity_donation_api.rb
+++ lib/polen_charity_donation_api.rb
@@ -1,15 +1,42 @@
 # frozen_string_literal: true
 
 require_relative "polen_charity_donation_api/version"
+require "rest-client"
+require "json"
 
-module PolenCharityDonationApi
-  class PolenCharityDonationApi
-    attr_reader :api_token, :base_url
-
-    # @param api_token token de acesso
-    def initialize(api_token)
-      @api_token = api_token.to_s
-      @base_url = "https://api.polen.com.br/api/v2"
-    end
+class PolenCharityDonationApi
+  attr_reader :api_token, :base_url
+  # @param api_token token de acesso
+  def initialize(api_token)
+    @api_token = api_token.to_s
+    @base_url = "https://api.polen.com.br/api/v2"
   end
+
+  #Causes
+
+  # Método que retorna todas as causas registradas
+  # @param params parametros de configuração
+  # @return Object
+  def get_all_cause(params={})
+    params[:api_token] = @api_token
+    RestClient.get("#{@base_url}/cause/all", params: params)
+  end
+
+  # Método que retorna todas as categorias de causas registradas
+  # @param params parametros de configuração
+  # @return Object
+  def get_cause_categories(params={})
+    params[:api_token] = @api_token
+    RestClient.get("#{@base_url}/cause/categories", params: params)
+  end
+
+  # Método que retorna todas as causas apoidas por uma loja
+  # @param params parametros de configuração
+  # @return Object
+  def get_own_cause(params)
+    params[:api_token] = @api_token
+    RestClient.get("#{@base_url}/cause", params: params)
+  end
+
 end
+
