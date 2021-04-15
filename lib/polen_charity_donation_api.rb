9775e505900667f0baeac37991c296977270f544
jeovane16 <jeovane.araujo@opolen.com.br>
2021-04-15 14:14:46 -0300

    adionado o método construtor

--- lib/polen_charity_donation_api.rb
+++ lib/polen_charity_donation_api.rb
@@ -3,7 +3,13 @@
 require_relative "polen_charity_donation_api/version"
 
 module PolenCharityDonationApi
-  class Error < StandardError; end
-  # Your code goes here...
+  class PolenCharityDonationApi
+    attr_reader :api_token, :base_url
 
+    # @param api_token token de acesso
+    def initialize(api_token)
+      @api_token = api_token.to_s
+      @base_url = "https://api.polen.com.br/api/v2"
+    end
+  end
 end
